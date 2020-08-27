7c03905e4d010746d8d486c21a67caa21d27ff4b
David <git-david@bluekeys.eu>
2020-08-27 08:01:57 +0100

    I used TRAMP for the first time. Wow! Default mode was scp though...

--- init.el
+++ init.el
@@ -109,3 +109,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(hl-line ((t (:inherit highlight :background "gold")))))
+
+;;; TRAMP
+(setq tramp-default-method "ssh")
